74de3ee9ee661b854b4c10b315e58e9cf8831520
Phison Khankhang <phisonk@hotmail.com>
2020-02-28 20:11:38 +0700

    no message

--- Crush AR/Assets/Script/SurfaceChecker.cs
+++ Crush AR/Assets/Script/SurfaceChecker.cs
@@ -14,9 +14,18 @@ public class SurfaceChecker : MonoBehaviour
     public GameObject placementIndicator;
     public GameObject spawningObject;
     public GameObject dialogBox;
+    public string[] dialogList;
+    public int dialogIndex;
     // Start is called before the first frame update
     void Start()
     {
+        dialogIndex = -1;
+        dialogList[0] = "This is awkward but bare with me …";
+        dialogList[1] = "I have got something to tell you …";
+        dialogList[2] = "You are the only person I want to be with …";
+        dialogList[3] = "Every single day until the end of time";
+        dialogList[4] = "So …";
+        dialogList[5] = "Will you be my Valentine?";
 
     }
 
@@ -28,6 +37,18 @@ public class SurfaceChecker : MonoBehaviour
         dialogBox.GetComponent<DialogBox>().textMeshPro.GetComponent<TMPro.TextMeshPro>().text = "Hello";
         UpdatePlacementPose();
         UpdateTargetIndicator();
+        if (Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            if (dialogIndex == 5)
+            {
+
+            }
+            else
+            {
+                dialogIndex++;
+                dialogBox.GetComponent<DialogBox>().textMeshPro.GetComponent<TMPro.TextMeshPro>().text = dialogList[dialogIndex];
+            }
+        }
         if (canPlace && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             // now the player touches the screen, spawn the prefab
